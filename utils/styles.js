9eebabcda3ab42d9f703466b1014a2281afb597a
StuartFleishman <Stuart.Alex.Fleishman@gmail.com>
2021-10-05 08:56:48 -0400

    styled navbar

---
+++ utils/styles.js
@@ -0,0 +1,12 @@
+import { makeStyles } from '@material-ui/core'
+
+const useStyles = makeStyles({
+  navbar: {
+    backgroundColor: '#203040',
+    '& a': {
+      color: '#ffffff',
+      marginLeft: 10,
+    },
+  },
+})
+export default useStyles;
