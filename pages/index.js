c0eb990d5b59c096bd48dfccc364d4e93b85bfb0
StuartFleishman <Stuart.Alex.Fleishman@gmail.com>
2021-10-10 10:28:19 -0400

    using lean to convert to POJO, which then using function in to convert to string

--- pages/index.js
+++ pages/index.js
@@ -9,44 +9,58 @@ import {
   CardContent,
   Typography,
 } from '@material-ui/core';
-import NextLink from 'next/link'
+import NextLink from 'next/link';
 import data from '../utils/data';
+import db from '../utils/db';
+import Product from '../models/Product';
 
-export default function Home() {
-  console.log(data.products)
+export default function Home(props) {
+  const { products } = props;
+
   return (
     <>
-    <Layout>
-      <div>
-        <h1>Products</h1>
-        <Grid container spacing={3}>
-          {data.products.map((product) => (
+      <Layout>
+        <div>
+          <h1>Products</h1>
+          <Grid container spacing={3}>
+            {products.map((product) => (
               <Grid item md={4} key={product.name}>
-              <Card>
-                <NextLink href={`/product/${product.slug}`} passHref>
-                <CardActionArea>
-                  <CardMedia
-                    component="img"
-                    image={product.image}
-                    title={product.name}
-                  ></CardMedia>
-                  <CardContent>
-                    <Typography>{product.name}</Typography>
-                  </CardContent>
-                </CardActionArea>
-                </NextLink>
-                <CardActions>
-                  <Typography>${product.price}</Typography>
-                  <Button size="small" color="primary">
-                    Add to Cart
-                  </Button>
-                </CardActions>
-              </Card>
+                <Card>
+                  <NextLink href={`/product/${product.slug}`} passHref>
+                    <CardActionArea>
+                      <CardMedia
+                        component="img"
+                        image={product.image}
+                        title={product.name}
+                      ></CardMedia>
+                      <CardContent>
+                        <Typography>{product.name}</Typography>
+                      </CardContent>
+                    </CardActionArea>
+                  </NextLink>
+                  <CardActions>
+                    <Typography>${product.price}</Typography>
+                    <Button size="small" color="primary">
+                      Add to Cart
+                    </Button>
+                  </CardActions>
+                </Card>
               </Grid>
-          ))}
-        </Grid>
-      </div>
-    </Layout>
+            ))}
+          </Grid>
+        </div>
+      </Layout>
     </>
   );
+}
+
+export async function getServerSideProps() {
+  await db.connect();
+  const products = await Product.find({}).lean();
+  await db.disconnect();
+  return {
+    props: {
+      products: products.map(db.convertDocToObj)
+    },
+  };
 }
