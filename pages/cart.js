ea0b2708684681bade349952914f1a601992eb47
StuartFleishman <Stuart.Alex.Fleishman@gmail.com>
2021-10-12 10:36:17 -0400

    built out row for shopping cart table body

--- pages/cart.js
+++ pages/cart.js
@@ -1,12 +1,28 @@
-import { Grid, Typography, Link} from '@material-ui/core';
+import {
+  Grid,
+  Typography,
+  Link,
+  TableContainer,
+  Table,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableBody,
+  Select,
+  MenuItem,
+  Button
+} from '@material-ui/core';
 import React, { useContext } from 'react';
+import Image from 'next/image';
 import Layout from '../components/Layout';
-import NextLink from 'next/link'
+import NextLink from 'next/link';
 import { Store } from '../utils/Store';
 
 export default function CartScreen() {
   const { state } = useContext(Store);
-  const { cart: {cartItems} } = state;
+  const {
+    cart: { cartItems },
+  } = state;
   return (
     <Layout title="Shopping Cart">
       <Typography component="h1" variant="h1">
@@ -19,10 +35,73 @@ export default function CartScreen() {
             <Link>Go shopping</Link>
           </NextLink>
         </div>
-      ) :  (
+      ) : (
         <Grid container spacing={1}>
           <Grid item md={9} xs={12}>
-            table of cart items
+            <TableContainer>
+              <Table>
+                <TableHead>
+                  <TableRow>
+                    <TableCell>image</TableCell>
+                    <TableCell>name</TableCell>
+                    <TableCell align="right">quantity</TableCell>
+                    <TableCell align="right">price</TableCell>
+                    <TableCell align="right">action</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {cartItems.map( item => (
+                    <TableRow key={item.id}>
+                      <TableCell>
+                        <NextLink href={`/product/${item.slug}`} passHref>
+                          <Link>
+                            <Image src={item.image}
+                            alt={item.name}
+                            width={50}
+                            height={50}>
+                            </Image>
+                          </Link>
+                        </NextLink>
+                      </TableCell>
+
+                      <TableCell>
+                        <NextLink href={`/product/${item.slug}`} passHref>
+                          <Link>
+                            <Typography>{item.name}</Typography>
+                          </Link>
+                        </NextLink>
+                      </TableCell>
+
+                      <TableCell align="right">
+                        <Select
+                          value={item.quantity}
+                          onChange={(e) =>
+                            updateCartHandler(item, e.target.value)
+                          }
+                        >
+                          {[...Array(item.countInStock).keys()].map((x) => (
+                            <MenuItem key={x + 1} value={x + 1}>
+                              {x + 1}
+                            </MenuItem>
+                          ))}
+                        </Select>
+                      </TableCell>
+
+                      <TableCell align="right">${item.price}</TableCell>
+                      <TableCell align="right">
+                        <Button
+                          variant="contained"
+                          color="secondary"
+                          onClick={() => removeItemHandler(item)}
+                        >
+                          x
+                        </Button>
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
           </Grid>
           <Grid md={3} xs={12}>
             cart actions
