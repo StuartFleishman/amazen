1ec8a13d889d778d45f4c63759bb9252f8f328ff
StuartFleishman <Stuart.Alex.Fleishman@gmail.com>
2021-10-12 12:55:04 -0400

    added link to register if user doesn't have account

--- pages/login.js
+++ pages/login.js
@@ -1,10 +1,18 @@
-import { Button, List, ListItem, TextField, Typography } from '@material-ui/core'
-import React from 'react'
-import Layout from '../components/Layout'
-import useStyles from '../utils/styles'
+import {
+  Button,
+  List,
+  ListItem,
+  TextField,
+  Typography,
+  Link,
+} from '@material-ui/core';
+import React from 'react';
+import Layout from '../components/Layout';
+import useStyles from '../utils/styles';
+import NextLink from 'next/link';
 
 export default function Login() {
-  const classes = useStyles()
+  const classes = useStyles();
   return (
     <Layout title="Login">
       <form className={classes.form}>
@@ -13,18 +21,36 @@ export default function Login() {
         </Typography>
         <List>
           <ListItem>
-            <TextField inputProps={{ type: 'email'}} variant="outlined" fullWidth id="email" label="Email">
-            </TextField>
+            <TextField
+              inputProps={{ type: 'email' }}
+              variant="outlined"
+              fullWidth
+              id="email"
+              label="Email"
+            ></TextField>
           </ListItem>
           <ListItem>
-            <TextField inputProps={{ type: 'password'}} variant="outlined" fullWidth id="password" label="Password">
-            </TextField>
+            <TextField
+              inputProps={{ type: 'password' }}
+              variant="outlined"
+              fullWidth
+              id="password"
+              label="Password"
+            ></TextField>
           </ListItem>
           <ListItem>
-            <Button></Button>
+            <Button variant="contained" type="submit" fullWidth color="primary">
+              Login
+            </Button>
+          </ListItem>
+          <ListItem>
+            Don't have an account? &nbsp;{' '}
+            <NextLink href="/register" passHref>
+              <Link>Register</Link>
+            </NextLink>
           </ListItem>
         </List>
       </form>
     </Layout>
-  )
+  );
 }
