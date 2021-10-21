8dd270855447bff6d3d316562b434a814e0d74a8
StuartFleishman <Stuart.Alex.Fleishman@gmail.com>
2021-10-21 12:48:52 -0400

    changed background of checkout wizard to transparent

--- components/checkoutWizard.js
+++ components/checkoutWizard.js
@@ -1,12 +1,14 @@
 import { Step, StepLabel, Stepper } from '@material-ui/core';
 import React from 'react';
-
+import useStyles from '../utils/styles';
 
 export default function CheckoutWizard({ activeStep = 0 }) {
-
+  const classes = useStyles();
   return (
     <Stepper
+      className={classes.transparentBackgroud}
       activeStep={activeStep}
+      alternativeLabel
     >
       {['Login', 'Shipping Address', 'Payment Method', 'Place Order'].map(
         (step) => (
