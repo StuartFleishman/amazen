60bc942e2056e5ca7279bdaac93496dd3c4db58e
StuartFleishman <Stuart.Alex.Fleishman@gmail.com>
2021-10-29 12:46:25 -0400

    added configuration to next.config for clourdinary files

--- next.config.js
+++ next.config.js
@@ -2,7 +2,8 @@ module.exports = {
   webpack5: true,
   webpack: (config) => {
     config.resolve.fallback = { fs: false };
-
     return config;
   },
+  reactStrictMode: true,
+  images: { domains: ['res.cloudinary.com'], formats: ['image/avif', 'image/webp'] },
 };
