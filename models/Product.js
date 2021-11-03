780675330eb83f1c5dd9b8178279f967e1705ce9
StuartFleishman <Stuart.Alex.Fleishman@gmail.com>
2021-11-03 08:53:52 -0400

    fixing model issue

--- models/Product.js
+++ models/Product.js
@@ -3,7 +3,7 @@ const User = require("./User")
 
 const reviewSchema = new mongoose.Schema(
   {
-    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: User, required: true },
     name: { type: String, required: true },
     rating: { type: Number, default: 0 },
     comment: { type: String, required: true },
