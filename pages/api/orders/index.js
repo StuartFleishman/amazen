766e54f011457e54823c4956413e245fe80deb74
StuartFleishman <Stuart.Alex.Fleishman@gmail.com>
2021-11-06 14:04:14 -0400

    fixing code

--- pages/api/orders/index.js
+++ pages/api/orders/index.js
@@ -1,14 +1,14 @@
 
 import nc from 'next-connect';
 import Order from '../../../models/Order';
-// import { isAuth } from '../../../utils/auth';
+import { isAuth } from '../../../utils/auth';
 import db from '../../../utils/db';
 import { onError } from '../../../utils/error';
 
 const handler = nc({
   onError,
 });
-// handler.use(isAuth);
+handler.use(isAuth);
 
 handler.post(async (req, res) => {
   await db.connect();
